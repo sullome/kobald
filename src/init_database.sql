c4e7249e6167d2aa6311886f831ab8bbaa8855c7
Sullome <sullome@yandex.com>
2018-03-05 14:15:49 +0300

    Add map data retrieval.

--- src/init_database.sql
+++ src/init_database.sql
@@ -4,6 +4,39 @@ create table tile_images(name TEXT, image BLOB);
 --}}}
 
 --{{{ Inserts
+insert into cards(ctype, tiles) values
+    ('field',  readfile('data/cards/field1')         ),
+    ('field',  readfile('data/cards/field2')         ),
+    ('field',  readfile('data/cards/field3')         ),
+    ('field',  readfile('data/cards/field4')         ),
+    ('field',  readfile('data/cards/field5')         ),
+    ('field',  readfile('data/cards/field6')         ),
+    ('field',  readfile('data/cards/field7')         ),
+    ('field',  readfile('data/cards/field8')         ),
+    ('field',  readfile('data/cards/field9')         ),
+    ('field',  readfile('data/cards/field10')        ),
+    ('field',  readfile('data/cards/field11')        ),
+    ('field',  readfile('data/cards/field12')        ),
+    ('field',  readfile('data/cards/field13')        ),
+    ('field',  readfile('data/cards/field14')        ),
+    ('field',  readfile('data/cards/field15')        ),
+    ('field',  readfile('data/cards/field16')        ),
+    ('field',  readfile('data/cards/field17')        ),
+    ('field',  readfile('data/cards/field18')        ),
+
+    ('end',    readfile('data/cards/end_entry')      ),
+    ('end',    readfile('data/cards/end_children')   ),
+    ('end',    readfile('data/cards/end_body')       ),
+    ('end',    readfile('data/cards/end_lair')       ),
+    ('end',    readfile('data/cards/end_item')       ),
+    ('end',    readfile('data/cards/end_rest')       ),
+
+    ('left',   readfile('data/cards/dead_end_left')  ),
+    ('top',    readfile('data/cards/dead_end_top')   ),
+    ('right',  readfile('data/cards/dead_end_right') ),
+    ('bottom', readfile('data/cards/dead_end_bottom')),
+
+    ('corner', readfile('data/cards/corner')         );
 --insert into tile_images(name, image) values
     --('filename.filetype', readfile('data/img/filepath')),
     --('filename.filetype', readfile('data/img/filepath'))
--- src/lib.rs
+++ src/lib.rs
@@ -2,15 +2,25 @@ extern crate rusqlite;
 extern crate rand;
 
 mod map {
+    use std::collections::HashMap;
+    use std::path::PathBuf;
     use rand::{Rng, StdRng};
+    use rusqlite::{Connection, DatabaseName, OpenFlags};
+    use std::io::Read;
 
     const CARDS_FIELDS_COUNT: usize = 18;
-    const CARDS_ENDS_COUNT:   usize = 5;
+    const CARDS_ENDS_COUNT:   usize = 6;
 
     const CARDS_MAP_SIDE:     usize = 3;
 
+    const DB_FILENAME: &'static str             = "data";
+    const DB_TABLE_W_CARDS: &'static str        = "cards";
+    const DB_TABLE_W_CARDS_COLUMN: &'static str = "tiles";
+
+    //{{{ Tile
     #[derive(Copy,Clone)]
-    enum Tile_Type {
+    enum TileType {
+        Start,
         Floor,
         Wall,
         Obstacle,
@@ -19,62 +29,204 @@ mod map {
 
     #[derive(Clone)]
     struct Tile {
-        ttype: Tile_Type,
+        ttype: TileType,
         passable: bool,
 
         // Name of the icon
         icon: String
     }
+    //}}}
 
+    //{{{ Card
     #[derive(Clone)]
-    struct Card (
+    struct Card {
         // Row of columns!!!
         // tiles[x][y]
-        Vec<Vec<Tile>>
-    );
+        tiles: Vec<Vec<Tile>>
+    }
+    impl Card {
+        pub fn new(id: i64, db_conn: &Connection) -> Result<Card, ()> {
+            if let Ok(mut tiles_blob) = db_conn.blob_open(
+                DatabaseName::Main,
+                DB_TABLE_W_CARDS,
+                DB_TABLE_W_CARDS_COLUMN,
+                id,
+                true                    // Read-Only
+            ) {
+                let mut tiles_string = String::with_capacity(
+                    tiles_blob.size() as usize
+                );
+                if let Ok(_) = tiles_blob.read_to_string(&mut tiles_string) {
+                    let card_side: usize = tiles_string.lines().count();
+
+                    let mut tiles_chars: Vec<Vec<char>> =
+                        // Row of columns
+                        Vec::with_capacity(card_side)
+                    ;
+                    for _x in 0..card_side {
+                        // Columns
+                        let mut column: Vec<char> = vec!['z'; card_side];
+                        tiles_chars.push(column);
+                    }
+                    for (y, line) in tiles_string.lines().enumerate() {
+                        for (x, c) in line.chars().enumerate() {
+                            tiles_chars[x][y] = c;
+                        }
+                    }
+
+                    let mut card = Card {
+                        // Row of columns
+                        tiles: Vec::with_capacity(card_side),
+                    };
+                    for x in 0..card_side {
+                        // Columns themselves
+                        card.tiles.push(Vec::with_capacity(card_side));
+
+                        for y in 0..card_side {
+                            let tile_char: char = tiles_chars[x][y];
+                            let tile: Tile = match tile_char {
+                                '#' => Tile {
+                                    ttype: TileType::Wall,
+                                    passable: false,
+                                    icon: String::from("wall.png"),
+                                },
+                                '_' => Tile {
+                                    ttype: TileType::Floor,
+                                    passable: true,
+                                    icon: String::from("floor.png"),
+                                },
+                                's' => Tile {
+                                    ttype: TileType::Start,
+                                    passable: true,
+                                    icon: String::from("floor.png"),
+                                },
+                                'x' => Tile {
+                                    ttype: TileType::Obstacle,
+                                    passable: false,
+                                    icon: String::from("floor.png"),
+                                },
+                                '?' => Tile {
+                                    ttype: TileType::Curiousity,
+                                    passable: true,
+                                    icon: String::from("floor.png"),
+                                },
+                                _   => Tile {
+                                    ttype: TileType::Floor,
+                                    passable: true,
+                                    icon: String::from("floor.png"),
+                                },
+                            };
+                            card.tiles[x].push(tile);
+                        }
+                    }
+                    Ok(card)
+                } else {
+                    Err(())
+                }
+            } else {
+                Err(())
+            }
+        }
+    }
+    //}}}
 
     /*
-     * Basic initialization handled by this function
+     * Basic initialization of cards is handled by this function
      */
-    fn init() {
-        let field_cards:   Vec<Card> = Vec::with_capacity(CARDS_FIELDS_COUNT);
-        let end_cards:     Vec<Card> =
-            // We need to fully cover ends "border"
-            // But exclude corners
-            if CARDS_MAP_SIDE * 4 > CARDS_ENDS_COUNT {
-                Vec::with_capacity(CARDS_MAP_SIDE * 4)
-            } else {
-                Vec::with_capacity(CARDS_ENDS_COUNT)
-            }
+    fn init_cards //{{{
+        () -> Result<
+            (Vec<Card>, Vec<Card>, HashMap<&'static str, Card>, Card),
+            ()
+        >
+    {
+        // Generic query for retrieving rowids of cards
+        let query = String::from("select rowid from ")
+            + DB_TABLE_W_CARDS
+            + " where ctype like ?;"
         ;
-        let dead_end_card: Card;
-        let corner_card:   Card;
 
-        /*
-         * We need to push dead ends to the end_cards,
-         * so that amount of end cards is equal (or greater)
-         * to the end locations.
-         *
-         * if CARDS_MAP_SIDE * 4 > CARDS_ENDS_COUNT {
-         *     for i in 0..{CARDS_MAP_SIDE * 4 - CARDS_ENDS_COUNT} {
-         *         end_cards.push(dead_end_card);
-         *     }
-         * }
-         */
+        let mut db_path = PathBuf::from(".");
+        db_path.push(DB_FILENAME);
+
+        let flags = OpenFlags::SQLITE_OPEN_READ_ONLY;
+        match Connection::open_with_flags(&db_path, flags) {
+            Ok(db_connection) => {
+                match db_connection.prepare(&query) {
+                    Ok(mut query_statement) => {
+                        let field_cards: Vec<Card>;
+                        let end_cards:   Vec<Card>;
+                        let mut dead_ends:   HashMap<&'static str, Card> =
+                            HashMap::with_capacity(4);
+                        let corner_card: Card;
+
+                        // Retrieving field cards
+                        field_cards = query_statement.query_map(
+                            &[&"field"],
+                            |row| {
+                                let rowid: i64 = row.get(0);
+                                Card::new(rowid, &db_connection).unwrap()
+                            }
+                        ).unwrap().map(
+                            |row| row.unwrap()
+                        ).collect();
+
+                        // Retrieving end cards
+                        end_cards = query_statement.query_map(
+                            &[&"end"],
+                            |row| {
+                                let rowid: i64 = row.get(0);
+                                Card::new(rowid, &db_connection).unwrap()
+                            }
+                        ).unwrap().map(
+                            |row| row.unwrap()
+                        ).collect();
+
+                        // Retrieving dead end cards
+                        for side in ["left", "top", "right", "bottom"].iter() {
+                            let rowid: i64 = query_statement
+                                .query_row(
+                                    &[side],
+                                    |row| row.get(0)
+                                )
+                                .unwrap()
+                            ;
+                            dead_ends.insert(
+                                side,
+                                Card::new(rowid, &db_connection).unwrap()
+                            );
+                        }
+
+                        // Retrieving corner card
+                        corner_card = query_statement.query_row(
+                            &[&"corner"],
+                            |row| {
+                                let rowid: i64 = row.get(0);
+                                Card::new(rowid, &db_connection).unwrap()
+                            }
+                        ).unwrap();
+
+                        Ok((field_cards, end_cards, dead_ends, corner_card))
+                    },
+                    Err(_) => return Err(())
+                }
+            },
+            Err(_) => return Err(())
+        }
     }
+    //}}}
 
     /*
      * This function generates random field of cards
      */
-    fn generate_field(
+    fn generate_field //{{{
+    (
         fields: &mut Vec<Card>, ends: &mut Vec<Card>,
-        dead_end: &Card, corner: &Card
+        dead_ends: &HashMap<&str, Card>, corner: &Card
     ) -> Vec<Vec<Card>>
     {
         let mut random_number_generator = StdRng::new()
             .expect("Failed to read randomness from operating system.");
         random_number_generator.shuffle(fields);
-        random_number_generator.shuffle(ends);
 
         // Ends makes a "border" of width equal to 1 card around fields
         let cardfield_side: usize = CARDS_MAP_SIDE + 2;
@@ -92,29 +244,26 @@ mod map {
             for y in 0..cardfield_side {
                 match (x, y) {
                     // Catch corners
-                    (0, 0) => cardfield[x].push(corner.clone()), // Patterns TODO
-                      (coord, 0)
-                    | (0, coord)
-                    | (coord, coord)
-                    if coord == end_index
-                    => cardfield[x].push(corner.clone()),
+                    (0, 0)
+                        => cardfield[x].push(corner.clone()),
+                    (coord, 0) | (0, coord)
+                        if coord == end_index
+                        => cardfield[x].push(corner.clone()),
+                    (coordx, coordy)
+                        if (coordx == end_index && coordy == end_index)
+                        => cardfield[x].push(corner.clone()),
 
                     // Then catch ends
-                      (0, ..)
-                    | (.., 0)
-                    => {
-                        if let Some(end) = ends.pop() {
-                            cardfield[x].push(end)
-                        }
-                    },
-                      (coord, ..)
-                    | (.., coord)
-                    if coord == end_index
-                    => {
-                        if let Some(end) = ends.pop() {
-                            cardfield[x].push(end)
-                        }
-                    },
+                    (0, ..)
+                        => cardfield[x].push(dead_ends["left"].clone()),
+                    (.., 0)
+                        => cardfield[x].push(dead_ends["top"].clone()),
+                    (coord, ..)
+                        if coord == end_index
+                        => cardfield[x].push(dead_ends["right"].clone()),
+                    (.., coord)
+                        if coord == end_index
+                        => cardfield[x].push(dead_ends["bottom"].clone()),
 
                     // Now we can catch everything else - main field
                     _ => {
@@ -125,22 +274,51 @@ mod map {
                 }
             }
         }
+
+        // Now placing special ends
+        let mut possible_locations: Vec<(usize, usize)> = Vec::with_capacity(
+            CARDS_MAP_SIDE * 4
+        );
+        for x in 1..end_index {
+            // Top row, without corners
+            possible_locations.push((x, 0));
+
+            // Bottom row, without corners
+            possible_locations.push((x, end_index));
+        }
+        for y in 1..end_index {
+            // Left column, without corners
+            possible_locations.push((0, y));
+
+            // Right column, without corners
+            possible_locations.push((end_index, y));
+        }
+
+        random_number_generator.shuffle(&mut possible_locations);
+        random_number_generator.shuffle(ends);
+        for (&(x, y), end) in possible_locations.iter().zip(ends.iter()) {
+            cardfield[x][y] = end.clone();
+        }
+
         cardfield
     }
+    //}}}
 
     /*
      * This funciton takes field of cards
      * and translates it into the game map
      */
-    fn generate_map(cards_field: &Vec<Vec<Card>>) -> Vec<Vec<Tile>> {
+    fn generate_map //{{{
+        (cards_field: &Vec<Vec<Card>>) -> Vec<Vec<Tile>>
+    {
         // Cards are square and equal. This is a must.
-        let corner_card = cards_field[0][0];
-        let card_side = len(corner_card);
-        let field_side = len(cards_field);
+        let corner_card = &cards_field[0][0];
+        let card_side = corner_card.tiles.len();
+        let field_side = cards_field.len();
 
         // Declare default tile
-        let tile_wall = Tile{
-            ttype: Tile_Type::Wall,
+        let tile_wall = Tile {
+            ttype: TileType::Wall,
             passable: false,
             icon: String::from("wall.png"),
         };
@@ -152,26 +330,30 @@ mod map {
         let mut map: Vec<Vec<Tile>> = Vec::with_capacity(map_side);
 
         // Basic initialization
-        for x in 0..map_side {
-            let column = vec![tile_wall; map_side];
+        for _x in 0..map_side {
+            let mut column: Vec<Tile> = Vec::with_capacity(map_side);
+            for _y in 0..map_side {
+                column.push(tile_wall.clone());
+            }
             map.push(column);
         }
 
         // Feeding actual tiles
-        for (field_x, &field_column) in cards_field.iter().enumerate() {
+        for (field_x, field_column) in cards_field.iter().enumerate() {
             for (field_y, field) in field_column.iter().enumerate() {
                 let offset_x: usize = field_x * field_side;
                 let offset_y: usize = field_y * field_side;
 
-                for (x, card_column) in field.iter().enumerate() {
+                for (x, card_column) in field.tiles.iter().enumerate() {
                     for (y, tile) in card_column.iter().enumerate() {
-                        map[offset_x + x][offset_y + y] = tile;
+                        map[offset_x + x][offset_y + y] = tile.clone();
                     }
                 }
             }
         }
         map
     }
+    //}}}
 }
 
 #[cfg(test)]
